1c367b5db6af93186a65860af0718826c38df8da
unknown <Veranika_Huzava@EPBYGOMW0527.gomel.epam.com>
2016-03-28 11:56:25 +0300

    Added delete for all elements (!!only 1 object) Added resize input Added refocus from div to input

--- Task_3_TaggleField/js/main.js
+++ Task_3_TaggleField/js/main.js
@@ -1,58 +1,65 @@
-
 var tags = [];
-tags = document.querySelectorAll("tag");
-document.getElementById('toggle-input').onkeypress = function(e){
-    if (!e) e = window.event;
-    var keyCode = e.keyCode || e.which;
-    if (keyCode == '13'){
-      // Enter pressed
-      addElement(this.parentElement, this, this.value);
-      this.value='';
-     // alert("fghfhfhfhfhfg");
-      return false;
-    }
-}
-
 
 Array.prototype.forEach.call(document.querySelectorAll('.input-textarea'), function(el) {
-  var input = el.querySelector('input');
+	var input = el.querySelector('input');
+	
+	conditionallyHideClearIcon();
+	input.addEventListener('input', conditionallyHideClearIcon);
+  
+	el.addEventListener('click', function(e) {
+		input.focus();
+		
+	});
 
-  conditionallyHideClearIcon();
-  input.addEventListener('input', conditionallyHideClearIcon);
-  el.querySelector('[data-clear-input]').addEventListener('click', function(e) {
-    input.value = '';
-    conditionallyHideClearIcon();
-  });
+	input.onkeypress = input.onkeydown = input.onkeyup = function() {
+		this.size = ( this.value.length > 1) ? this.value.length : 1;
+	};
+	
+	input.onfocus = function() {
+		if(!this.value) {
+			this.size = 1;
+		}
+	}
+	
+	input.onblur = function() {
+		this.removeAttribute("size");
+	}
+  
+	el.querySelector('[data-clear-input]').addEventListener('click', function(e) {
+		input.value = '';
+		var temp = document.getElementById("container");
+		temp.remove();
+		tags = [];
+		conditionallyHideClearIcon();
+	});
 
-  function conditionallyHideClearIcon(e) {
-    var target = (e && e.target) || input;
-    target.nextElementSibling.style.display = target.value ? 'block' : 'none';
-  }
+	function conditionallyHideClearIcon(e) {
+		var target = (e && e.target) || input;
+		target.nextElementSibling.style.display = target.value ? 'block' : 'none';
+	}
 });
 
-
-function addElement(parent, child, value) {
-    var span = document.createElement("span");
-    span.setAttribute('class',"tag");
-    var node = document.createTextNode(value);
-    span.appendChild(node);
-
-    parent.insertBefore(span,child);
+	//tags = document.querySelectorAll("tag");
+	document.getElementById('input-area').onkeypress = function(e){
+		if (!e) e = window.event;
+		var keyCode = e.keyCode || e.which;
+		if (keyCode == '13' && this.value){
+		  // Enter pressed
+			addElement(this.previousElementSibling, this, this.value);
+			this.value='';
+			this.size = 1;
+			return false;
+    }
 }
+	//добавить крестик в спан
+	function addElement(parent, child, value) {
+		var span = document.createElement("span");
+		span.setAttribute('class',"tag");
+		var node = document.createTextNode(value);
+		span.appendChild(node);
+
+	   tags.push(parent.appendChild(span));
+	}
 
 
-/*function detectEnterPressed(e) {
-    var characterCode
-    if(e && e.which){ // NN4 specific code
-        e = e
-        characterCode = e.which
-    }
-    else {
-        e = event
-        characterCode = e.keyCode // IE specific code
-    }
-    if (characterCode == 13){
-        return true;
-    } // Enter key is 13
-    else return false;
-}*/
+
