5a3b0e9ea7f83f1290040a5ebd1406315952dcc5
unknown <Veranika_Huzava@EPBYGOMW0527.gomel.epam.com>
2016-03-29 14:51:44 +0300

    code refactoring

--- Task_3_TaggleField/js/main.js
+++ Task_3_TaggleField/js/main.js
@@ -1,6 +1,12 @@
 var tags = [];
 var inputTextarea = document.querySelectorAll('.input-textarea');
-Array.prototype.forEach.call(inputTextarea , function(el) {
+
+for(var i=0; i< inputTextarea.length; i++){
+   createElements(inputTextarea[i]);
+};
+
+
+Array.prototype.forEach.call(inputTextarea, function(el) {
 	var input = el.querySelector('input');
 	
 	conditionallyHideClearIcon();
@@ -8,23 +14,8 @@ Array.prototype.forEach.call(inputTextarea , function(el) {
   
 	el.addEventListener('click', function(e) {
 		input.focus();
-		
 	});
 
-	input.onkeypress = input.onkeydown = input.onkeyup = function() {
-		this.size = ( this.value.length > 1) ? this.value.length : 1;
-	};
-	
-	input.onfocus = function() {
-		if(!this.value) {
-			this.size = 1;
-		}
-	}
-	
-	input.onblur = function() {
-		this.removeAttribute("size");
-	}
-  
 	el.querySelector('[data-clear-input]').addEventListener('click', function(e) {
         removeAllElements(this);
 		conditionallyHideClearIcon();
@@ -37,10 +28,8 @@ Array.prototype.forEach.call(inputTextarea , function(el) {
 
 	function removeAllElements(target) {
         input.value = '';
-
         //var temp =  target.parentNode.getElementById("container");
         var temp =  target.parentNode.firstElementChild;
-
         while (temp.hasChildNodes()) {
             temp.removeChild(temp.lastChild);
         }
@@ -48,50 +37,79 @@ Array.prototype.forEach.call(inputTextarea , function(el) {
 	};
 });
 
+    function createElements (parent) {
+        var spanContainer = document.createElement('span');
+        spanContainer.setAttribute('id', 'container');
+        parent.appendChild(spanContainer);
 
-    var len = inputTextarea.length;
-    for(var i=0;i<len;i++ ) {
-        inputTextarea[i].firstElementChild.nextElementSibling.onkeypress = function(e){
+        var inputEl = document.createElement('input');
+        inputEl.setAttribute('id', 'input-area');
+        inputEl.setAttribute('class', 'toggle-input');
+        inputEl.setAttribute('type', 'text');
+        inputEl.setAttribute('placeholder', 'Enter text');
+        inputEl.setAttribute('maxlength', '15');
+        parent.appendChild(inputEl);
+
+        inputEl.addEventListener('keypress', function(e) {
             if (!e) e = window.event;
             var keyCode = e.keyCode || e.which;
             if (keyCode == '13' && this.value){
-                // Enter pressed
-                addElement(this.previousElementSibling, this, this.value);
-                this.value='';
-                this.size = 1;
-                return false;
+                 // Enter pressed
+                 addElement(this.previousElementSibling, this, this.value);
+                 this.value='';
+                 this.size = 1;
+                 return false;
             }
-        };
+        });
+
+        inputEl.addEventListener('keypress',resizeInput);
+        inputEl.addEventListener('keyup',resizeInput);
+        inputEl.addEventListener('keydown',resizeInput);
+
+        inputEl.addEventListener('focus',function() {
+            if(!this.value) {
+                this.size = 1;
+            }
+        });
+
+        inputEl.addEventListener('blur',function() {
+        	this.removeAttribute("size");
+        });
+
+        var spanClearAll = document.createElement('span');
+        spanClearAll.setAttribute('data-clear-input','');
+        // var textNode = document.createTextNode("x");
+         // spanClearAll.appendChild(textNode);
+        spanClearAll.innerHTML = "&times;";
+        parent.appendChild(spanClearAll);
     };
 
+    function resizeInput() {
+        this.size = ( this.value.length > 1) ? this.value.length : 1;
+    };
 
-	//добавить крестик в спан
 	function addElement(parent, child, value) {
-		var span = document.createElement("span");
-		span.setAttribute('class',"tag");
 		var spanChild = document.createElement("span");
-		var spanX = document.createElement("span");
-		spanX.setAttribute('class',"remove");
-        spanX.setAttribute('onClick',"removeCurrent(this)");
-
-		var nodeX = document.createTextNode('x');
-
-
 		var node = document.createTextNode(value);
 		spanChild.appendChild(node);
-		spanX.appendChild(nodeX);
+
+		var spanX = document.createElement("span");
+		spanX.setAttribute('class',"remove");
+		//var nodeX = document.createTextNode('x');
+        //spanX.appendChild(nodeX);
+        spanX.innerHTML = "&times;";
+
+        var span = document.createElement("span");
+        span.setAttribute('class',"tag");
 		span.appendChild(spanChild);
 		span.appendChild(spanX);
 
 	    tags.push(parent.appendChild(span));
 
-      //  spanX.onClick = removeCurrent(spanX);
-
-	  // span.lastChild.addEventListener('click', removeCurrent(this));
-	 // parent.lastChild.lastChild.addEventListener('click', removeCurrent(parent.lastChild));
+	    spanX.addEventListener('click', removeCurrent);
 	};
 
 
-    function removeCurrent(el) {
-        el.parentNode.remove();
+    function removeCurrent() {
+        this.parentNode.remove();
     };
